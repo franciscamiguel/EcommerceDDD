375eec3937f791d87103b6a2548dda6871b5f84c
francisca92firmino <francisca.miguel@woli.tec.br>
2022-03-05 18:08:25 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/Entities/Produto.cs
@@ -0,0 +1,30 @@
+﻿using Entities.Notifications;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Entities.Entities
+{
+    [Table("Product")]
+   public class Produto : Notifies
+    {
+        [Column("PRD_Id")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Column("PRD_Nome")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+        [Column("PRD_Valor")]
+        [Display(Name = "Valor")]
+        public decimal Valor { get; set; }
+
+
+        [Column("PRD_Estado")]
+        [Display(Name = "Estado")]
+        public bool Estado { get; set; }
+    }
+}
---
+++ Entities/Notifications/Notifies.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Entities.Notifications
+{
+   public class Notifies
+    {
+        public Notifies()
+        {
+            Notitycoes = new List<Notifies>();
+        }
+
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+
+        [NotMapped]
+        public string Mensagem { get; set; }
+
+        [NotMapped]
+        public List<Notifies> Notitycoes;
+
+        public bool validarpropriedadeString(  string valor, string nomepropriedade)
+        {
+            if(string.IsNullOrWhiteSpace(valor) ||string.IsNullOrWhiteSpace(nomepropriedade))
+            {
+                Notitycoes.Add(new Notifies
+                {
+                    Mensagem = "Campo Obrigatorio",
+                    NomePropriedade = nomepropriedade
+                });
+
+                return false;
+            }
+            return true;
+        }
+
+
+        public bool validarpropriedadeInt(int valor, string nomepropriedade)
+        {
+            if (valor < 1 || string.IsNullOrWhiteSpace(nomepropriedade))
+            {
+                Notitycoes.Add(new Notifies
+                {
+                    Mensagem = "O valor deve ser maior que 0",
+                    NomePropriedade = nomepropriedade
+                });
+
+                return false;
+            }
+            return true;
+        }
+
+
+        public bool validarpropriedadeDecimal(decimal valor, string nomepropriedade)
+        {
+            if (valor < 1 || string.IsNullOrWhiteSpace(nomepropriedade))
+            {
+                Notitycoes.Add(new Notifies
+                {
+                    Mensagem = "O valor deve ser maior que 0",
+                    NomePropriedade = nomepropriedade
+                });
+
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+}
